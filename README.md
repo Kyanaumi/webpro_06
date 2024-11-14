aabb6e40faeb5de69819c92563cd7c7e8a29c84a
Kiyoto Kanaumi <usa235kiyoo@gmail.com>
2024-11-14 21:06:34 +0900

    内容の追加

--- README.md
+++ README.md
@@ -1,24 +1,53 @@
 # webpro_06
-10月29日
+11月12日
 ## このプログラムについて
 ## ファイル一覧
 ファイル名 | 説明
 -|-
 app5.js | プログラム全体
+public/test.html | htmlのテスト用の画面
 public/janken.html | じゃんけんの開始画面
-views/janken.ejs | じゃんけんのテンプレートファイル
+views/show.ejs | Hello WorldとBon jourが表示されるテンプレートファイル
+views/icon.ejs | Appleのロゴが表示されるテンプレートファイル
+views/luck.ejs | おみくじができるテンプレートファイル
+views/janken.ejs | じゃんけんをするテンプレートファイル
+views/nibuiti.ejs | 2分の1ををてるゲームのテンプレートファイル
+views/attimuite.ejs | コンピュータとあっち向いてホイをするためのテンプレートファイル
+
 ```javascript
 console.log("Hello");
 ```
+
 ## 使用方法
-1. ターミナルを開き```node app5.js```を起動する
-1. webブラウザでlocalhost:8080/public/janken.htmlにアクセスする
-1. 自分の手を入力する
+### show.ejs
+1. ターミナルを開き```cd Desktop```，```cd webpuro```，```cd webpro_06```の順に実行する．
+1. ターミナル内で```node app5.js```を実行する．
+1. webブラウザ内で http://localhost:8080/hello1 または，http://localhost:8080/hello2 にアクセスする．
+1. Hello WorldとBon jourが表示される．
+
 
 ```mermaid
 flowchart TD;
-開始 --> 終了;
+
+開始 --> node1[文字の表示] -->終了;
 ```
+
+### icon.ejs
+1. ターミナルを開き```cd Desktop```，```cd webpuro```，```cd webpro_06```の順に実行する．
+1. ターミナル内で```node app5.js```を実行する．
+1. webブラウザ内で http://localhost:8080/icon にアクセスする．
+1. Appleのロゴが表示される．
+
+```mermaid
+flowchart TD;
+開始 --> node1[アイコンの表示] -->終了;
+```
+
+### luck.ejs
+1. ターミナルを開き```cd Desktop```，```cd webpuro```，```cd webpro_06```の順に実行する．
+1. ターミナル内で```node app5.js```を実行する．
+1. webブラウザ内で http://localhost:8080/luck にアクセスする．
+1. 運勢が大吉，中吉，小吉の中から選ばれる．
 
 ```mermaid
 flowchart TD;
@@ -26,12 +55,98 @@ flowchart TD;
 start["開始"];
 end1["終了"]
 if{"条件に合うか"}
+dai["大吉"]
+tyuu["中吉"]
+syou["小吉"]
+
+start --> if
+if -->|1| dai
+if --> |2| tyuu
+if -->|3| syou
+dai --> end1
+tyuu --> end1
+syou --> end1
+```
+
+### janken.ejs
+1.  ターミナルを開き```cd Desktop```，```cd webpuro```，```cd webpro_06```の順に実行する．
+1. ```node app5.js```を実行する
+1. webブラウザでlocalhost:8080/public/janken.htmlにアクセスする．
+1. 自分の手，グー，チョキ，パーのどれかを入力して送信する．
+
+```mermaid
+flowchart TD;
+
+start["開始"];
+end1["終了"]
+if{"グー，チョキ，パーのどれかを送信"}
+win["勝ち"]
+loose["負け"]
+aiko["引き分け"]
+
+start --> if
+if -->|グー| win
+if --> |チョキ| win
+if -->|パー| win
+win --> end1
+if -->|グー| loose
+if -->|チョキ| loose
+if -->|パー| loose
+loose--> end1
+if --> |グー| aiko
+if --> |チョキ| aiko
+if --> |パー| aiko
+aiko --> end1
+```
+
+### nibuiti.ejs
+1. ターミナルを開き```cd Desktop```，```cd webpuro```，```cd webpro_06```の順に実行する．
+1. ターミナル内で```node app5.js```を実行する．
+1. webブラウザ内で http://localhost:8080/nibuiti にアクセスする．
+1. コンピュータが右と左どちらなのかを予想し右，左のどちらかを入力し送信する．
+
+```mermaid
+flowchart TD;
+
+start["開始"];
+end1["終了"]
+if{"右左のどちらかを送信"}
 win["勝ち"]
 loose["負け"]
 
 start --> if
-if -->|yes| win
+if -->|右| win
+if --> |左| win
 win --> end1
-if -->|no| loose
+if -->|右| loose
+if -->|左| loose
+loose--> end1
+```
+
+### atiimuite.ejs
+1. ターミナルを開き```cd Desktop```，```cd webpuro```，```cd webpro_06```の順に実行する．
+1. ターミナル内で```node app5.js```を実行する．
+1. webブラウザ内で http://localhost:8080/attimuite にアクセスする．
+1. コンピュータが上，右，左，下のどちらの方向に向くのかを予想し上，右，左，下のどれかを入力し送信する．
+
+```mermaid
+flowchart TD;
+
+start["開始"];
+end1["終了"]
+if{"上,右,左,下のどれかを送信"}
+win["勝ち"]
+loose["負け"]
+
+start --> if
+if -->|上| win
+if --> |右| win
+if -->|左| win
+if --> |下| win
+win --> end1
+if -->|上| loose
+if -->|右| loose
+if -->|左| loose
+if -->|下| loose
 loose--> end1
 ```
--- app5.js
+++ app5.js
@@ -10,22 +10,31 @@ app.get("/hello1", (req, res) => {
   res.render('show', { greet1:message1, greet2:message2});
 });
 
+//http://localhost:8080/hello1
+
 app.get("/hello2", (req, res) => {
   res.render('show', { greet1:"Hello world", greet2:"Bon jour"});
 });
+
+//http://localhost:8080/hello2
 
 app.get("/icon", (req, res) => {
   res.render('icon', { filename:"./public/Apple_logo_black.svg", alt:"Apple Logo"});
 });
 
+//http://localhost:8080/icon
+
 app.get("/luck", (req, res) => {
-  const num = Math.floor( Math.random() * 6 + 1 );
+  const num = Math.floor( Math.random() * 3 + 1 );
   let luck = '';
   if( num==1 ) luck = '大吉';
   else if( num==2 ) luck = '中吉';
+  else luck = '小吉'
   console.log( 'あなたの運勢は' + luck + 'です' );
   res.render( 'luck', {number:num, luck:luck} );
 });
+
+//http://localhost:8080/luck
 
 app.get("/janken", (req, res) => {
   let hand = req.query.hand;
@@ -65,10 +74,89 @@ app.get("/janken", (req, res) => {
   res.render( 'janken', display );
 });
 
-app.listen(8080, () => console.log("Example app listening on port 8080!"));
 //http://localhost:8080/janken
 
 
+app.get("/nibuiti", (req, res) => {
+  let hand = req.query.hand;
+  let win = Number( req.query.win ||0);
+  let total = Number( req.query.total ||0);
+  console.log( {hand, win, total});
+  const num = Math.floor( Math.random() * 2 + 1 );
+  let cpu = '';
+  if( num==1 ) cpu = '右';
+  else if( num==2 ) cpu = '左';
+  
+  let judgement = '';
+  
+  if (cpu == '右' && hand == '右') judgement = '勝ち' ,win++
+  else if (cpu == '右' && hand == '左') judgement = '負け'
+  
+  if (cpu == '左' && hand == '右') judgement = '負け' 
+  else if (cpu == '左' && hand == '左') judgement = '勝ち',win++
+
+  total = total+1;
+  
+  const display = {
+    your: hand,
+    cpu: cpu,
+    judgement: judgement,
+    win: win,
+    total: total
+  }
+  res.render( 'nibuiti', display );
+});
+
+//http://localhost:8080/nibuiti
+
+app.get("/attimuite", (req, res) => {
+  let hand = req.query.hand;
+  let win = Number( req.query.win ||0);
+  let total = Number( req.query.total ||0);
+  console.log( {hand, win, total});
+  const num = Math.floor( Math.random() * 4 + 1 );
+  let cpu = '';
+  if( num==1 ) cpu = '上';
+  else if( num==2 ) cpu = '右';
+  else if ( num==3 ) cpu = '左';
+  else cpu = '下'
+  // ここに勝敗の判定を入れる
+  // 今はダミーで人間の勝ちにしておく
+  let judgement = '';
+ 
+  if (cpu == '上' && hand == '上') judgement = '勝ち' ,win++
+  else if (cpu == '上' && hand == '右') judgement = '負け'
+  else if (cpu == '上' && hand == '左') judgement = '負け'
+  else if (cpu == '上' && hand == '下') judgement = '負け'
+
+  if (cpu == '右' && hand == '上') judgement = '負け' 
+  else if (cpu == '右' && hand == '右') judgement = '勝ち' , win++
+  else if (cpu == '右' && hand == '左') judgement = '負け'
+  else if (cpu == '右' && hand == '下') judgement = '負け'
+
+  if (cpu == '左' && hand == '上') judgement = '負け' 
+  else if (cpu == '左' && hand == '右') judgement = '負け'
+  else if (cpu == '左' && hand == '左') judgement = '勝ち' , win++
+  else if (cpu == '左' && hand == '下') judgement = '負け'
+
+  if (cpu == '下' && hand == '上') judgement = '負け' 
+  else if (cpu == '下' && hand == '右') judgement = '負け'
+  else if (cpu == '下' && hand == '左') judgement = '負け'
+  else if (cpu == '下' && hand == '下') judgement = '勝ち', win++
+
+  total = total+1;
+  
+  const display = {
+    your: hand,
+    cpu: cpu,
+    judgement: judgement,
+    win: win,
+    total: total
+  }
+  res.render( 'attimuite', display );
+});
+app.listen(8080, () => console.log("Example app listening on port 8080!"));
+//http://localhost:8080/attimuite
 
 
 
